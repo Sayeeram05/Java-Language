4954514ce72b410ca250936a6da105edf1739e7e
Sayeeram05 <sr6172@srmist.edu.in>
2024-07-12 04:36:46 +0530

    Created Panels and TitleBar

--- Projects/Cash_Book/src/HomePage.java
+++ Projects/Cash_Book/src/HomePage.java
@@ -1,3 +1,139 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class HomePage {
+
+    //-----------------------------------------------L E F T P A N E L----------------------------------------------//
+
+    JLabel close = new JLabel(" X ");
+    JFrame frame = new JFrame();
+    JLabel logout = new JLabel("Logout");
+    JPanel rightpanel = new JPanel();
+    GradientPanel leftPanel = new GradientPanel(Color.decode("#004FF9"),Color.decode("#56CCF2"),0);
+    private static HomePage homePage; 
+
+    //-----------------------------------------------L E F T P A N E L----------------------------------------------//
+
+    public HomePage() {
+
+        //---------------------------------------------------F R A M E--------------------------------------------------//
+
+        // Defining frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // 
+        frame.setSize(1800,1000); // frame size
+        frame.setLocationRelativeTo(null);  // display frame in center of screen
+        frame.setUndecorated(true);
+        frame.setIconImage(new ImageIcon("lib\\images\\App_Icon.png").getImage());
+        frame.setLayout(null);
+
+        //----------------------------------------------R I G H T P A N E L---------------------------------------------//
+
+        // Initilize components used in rightpanel
+        JPanel menuBar = new JPanel();
+        close = new JLabel("X",JLabel.CENTER);
+        JLabel CashBookHeading = new JLabel();
+
+
+        // Defining right_Panel
+        rightpanel.setPreferredSize(new Dimension(1500, 1000));
+        rightpanel.setBackground(new Color(206, 230, 243));
+        rightpanel.setBounds(300,0,1500,1000);
+        rightpanel.setLayout(null);
+
+        // Defining MenuBar
+        menuBar.setPreferredSize(new Dimension(149, 40));
+        menuBar.setBounds(0, 0, 1500, 40);
+        menuBar.setBackground(Color.WHITE);
+        menuBar.setLayout(null);
+
+        // Defining close
+        close.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 25));
+        close.setBounds(1460, 0, 40, 40);
+        close.setOpaque(true);
+        close.setBackground(Color.WHITE);
+        close.setForeground(Color.RED);
+        close.addMouseListener(new java.awt.event.MouseAdapter() 
+        {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e)
+            {
+                closeclicked(e);
+            }
+
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e)
+            {
+                closeentered(e);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e)
+            {
+                closeexited(e);
+            }
+        });
+
+        // Defining CashBookHeading
+        CashBookHeading.setText("Cash Book");
+        CashBookHeading.setFont(new Font("Arial Black", Font.BOLD,25));
+        CashBookHeading.setForeground(new Color(6, 143, 255));
+        CashBookHeading.setBounds(665, 0, 200, 40);
+        CashBookHeading.addMouseListener(new java.awt.event.MouseAdapter() 
+        {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e)
+            {
+                   //homeclicked(e);
+            }
+        });
+
+
+        // Adding defined components to the MenuBar
+        menuBar.add(close);
+        menuBar.add(CashBookHeading);
+
+        // Adding defined components to the RightPanel
+        rightpanel.add(menuBar);
+
+        // Adding defined components to the Frame
+        frame.add(rightpanel);
+
+
+        frame.setVisible(true);
+
+    }
+
+
+
+    //--------------------------------------------A C T I O N L I S T E N E R-------------------------------------------//
+
+    public void closeclicked(java.awt.event.MouseEvent e)
+    {
+        frame.dispose();
+    }
+    public void closeentered(java.awt.event.MouseEvent e)
+    {
+        close.setForeground(Color.BLACK);
+        close.setBackground(Color.RED);
+    }
+    public void closeexited(java.awt.event.MouseEvent e)
+    {
+        close.setForeground(Color.RED);
+        close.setBackground(Color.WHITE);
+    }
+
+
+
+    public static void main(String[] args) {
+        homePage = new HomePage();
+    }
+    
+
+    
     
 }
