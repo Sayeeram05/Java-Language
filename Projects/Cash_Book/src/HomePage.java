dd01a117b6deed4fde298088454bc783d133fb83
Sayeeram05 <sr6172@srmist.edu.in>
2024-07-14 12:53:37 +0530

    Button Panels

--- Projects/Cash_Book/src/HomePage.java
+++ Projects/Cash_Book/src/HomePage.java
@@ -8,16 +8,15 @@ import javax.swing.JPanel;
 
 public class HomePage {
 
-    //-----------------------------------------------L E F T P A N E L----------------------------------------------//
+    //-----------------------------------------------G L O B A L----------------------------------------------//
 
     JLabel close = new JLabel(" X ");
     JFrame frame = new JFrame();
     JLabel logout = new JLabel("Logout");
     JPanel rightpanel = new JPanel();
     GradientPanel leftPanel = new GradientPanel(Color.decode("#004FF9"),Color.decode("#56CCF2"),0);
-    private static HomePage homePage; 
 
-    //-----------------------------------------------L E F T P A N E L----------------------------------------------//
+    //-----------------------------------------------C  O   N   S   T   R   U   C   T   O   R----------------------------------------------//
 
     public HomePage() {
 
@@ -34,10 +33,28 @@ public class HomePage {
         //----------------------------------------------R I G H T P A N E L---------------------------------------------//
 
         // Initilize components used in rightpanel
-        JPanel menuBar = new JPanel();
+        JPanel TitleBar = new JPanel();
         close = new JLabel("X",JLabel.CENTER);
         JLabel CashBookHeading = new JLabel();
 
+        JPanel Button_Panel = new JPanel();
+        JLabel Button_Panel_Title = new JLabel("Home");
+
+        GradientPanel Cash_In_Panel = new GradientPanel(Color.decode("#000000"), Color.decode("#004ff9"), 50);
+        JLabel Cash_In_icon = new JLabel();
+        JLabel Cash_In_label = new JLabel("CASH IN ");
+
+        GradientPanel Cash_Out_Panel = new GradientPanel(Color.decode("#0c0014"), Color.decode("#0779E4"), 50);
+        JLabel Cash_Out_icon = new JLabel();
+        JLabel Cash_Out_label = new JLabel("CASH OUT ");
+
+        GradientPanel Transaction_Panel = new GradientPanel(Color.decode("#1A2980"), Color.decode("#26D0CE"), 50);
+        JLabel Transaction_icon = new JLabel();
+        JLabel Transaction_label = new JLabel("HISTORY ");
+
+
+
+        JPanel Content_Panel = new JPanel();
 
         // Defining right_Panel
         rightpanel.setPreferredSize(new Dimension(1500, 1000));
@@ -45,11 +62,11 @@ public class HomePage {
         rightpanel.setBounds(300,0,1500,1000);
         rightpanel.setLayout(null);
 
-        // Defining MenuBar
-        menuBar.setPreferredSize(new Dimension(149, 40));
-        menuBar.setBounds(0, 0, 1500, 40);
-        menuBar.setBackground(Color.WHITE);
-        menuBar.setLayout(null);
+        // Defining TitleBar
+        TitleBar.setPreferredSize(new Dimension(149, 40));
+        TitleBar.setBounds(0, 0, 1500, 40);
+        TitleBar.setBackground(Color.WHITE);
+        TitleBar.setLayout(null);
 
         // Defining close
         close.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 25));
@@ -79,10 +96,10 @@ public class HomePage {
         });
 
         // Defining CashBookHeading
-        CashBookHeading.setText("Cash Book");
-        CashBookHeading.setFont(new Font("Arial Black", Font.BOLD,25));
-        CashBookHeading.setForeground(new Color(6, 143, 255));
-        CashBookHeading.setBounds(665, 0, 200, 40);
+        CashBookHeading.setText("CASH BOOK");
+        CashBookHeading.setFont(new Font("Arial Black", Font.BOLD,30));
+        CashBookHeading.setForeground(new Color(0, 121, 255));
+        CashBookHeading.setBounds(650, 0, 250, 40);
         CashBookHeading.addMouseListener(new java.awt.event.MouseAdapter() 
         {
             @Override
@@ -92,17 +109,94 @@ public class HomePage {
             }
         });
 
+        // Adding defined components to the TitleBar
+        TitleBar.add(close);
+        TitleBar.add(CashBookHeading);
 
-        // Adding defined components to the MenuBar
-        menuBar.add(close);
-        menuBar.add(CashBookHeading);
+        //----------------------------------------------B U T T O N P A N E L---------------------------------------------//
+
+        Button_Panel.setBounds(20, 75, 1462, 280);
+        Button_Panel.setBackground(new Color(206, 230, 243));// new Color(240, 240, 240)
+        Button_Panel.setLayout(null);
+
+        // Button Panel Title
+        Button_Panel_Title.setFont(new Font("Roboto", Font.BOLD, 35));
+        Button_Panel_Title.setForeground(new Color(6, 143, 255));
+        Button_Panel_Title.setBounds(10, 0, 200, 40);
+
+        // Cash In
+        Cash_In_Panel.setBounds(10, 60, 400, 200);
+        Cash_In_Panel.setLayout(null);
+        Cash_In_icon.setIcon(new ImageIcon("lib\\images\\PlusIcon.png"));
+        Cash_In_icon.setBounds(245, 50, 100, 100);
+
+        Cash_In_label.setFont(new Font("Roboto", Font.BOLD, 30));
+        Cash_In_label.setForeground(new Color(240, 240, 240));
+        Cash_In_label.setBounds(25, 50, 200, 30);
+
+
+        Cash_In_Panel.add(Cash_In_icon);
+        Cash_In_Panel.add(Cash_In_label);
+
+        Cash_Out_Panel.setBounds(525, 60, 400, 200);
+        Cash_Out_Panel.setLayout(null);
+        Cash_Out_icon.setIcon(new ImageIcon("lib\\images\\MinusIcon.png"));
+        Cash_Out_icon.setBounds(245, 50, 100, 100);
+
+        Cash_Out_label.setFont(new Font("Roboto", Font.BOLD, 30));
+        Cash_Out_label.setForeground(new Color(240, 240, 240));
+        Cash_Out_label.setBounds(25, 50, 200, 30);
+
+
+        Cash_Out_Panel.add(Cash_Out_icon);
+        Cash_Out_Panel.add(Cash_Out_label);
+
+        // Transaction
+        Transaction_Panel.setBounds(1050, 60, 400, 200);
+        Transaction_Panel.setLayout(null);
+        Transaction_icon.setIcon(new ImageIcon("lib\\images\\HistoryIcon.png"));
+        Transaction_icon.setBounds(245, 50, 100, 100);
+
+        Transaction_label.setFont(new Font("Roboto", Font.BOLD, 30));
+        Transaction_label.setForeground(new Color(240, 240, 240));
+        Transaction_label.setBounds(25, 50, 225, 30);
+
+
+        Transaction_Panel.add(Transaction_icon);
+        Transaction_Panel.add(Transaction_label);
+
+        // Adding defined components to the Button Panel
+        Button_Panel.add(Button_Panel_Title);
+        Button_Panel.add(Cash_In_Panel);
+        Button_Panel.add(Cash_Out_Panel);
+        Button_Panel.add(Transaction_Panel);
+        
+
+        //----------------------------------------------C O N T E N T P A N E L---------------------------------------------//
+
+        Content_Panel.setBounds(20, 400, 1462, 550);
+        Content_Panel.setBackground(new Color(240, 240, 240));//  new Color(206, 230, 243)
+        Content_Panel.setLayout(null);
 
         // Adding defined components to the RightPanel
-        rightpanel.add(menuBar);
+        rightpanel.add(TitleBar);
+        rightpanel.add(Button_Panel);
+        rightpanel.add(Content_Panel);
+
+
+        //----------------------------------------------L E F T P A N E L---------------------------------------------//
+
+        
+        leftPanel.setPreferredSize(new Dimension(300, 1000));
+        leftPanel.setBounds(0, 0, 300, 1000);
+        leftPanel.setLayout(null);
+
+        
+
 
         // Adding defined components to the Frame
         frame.add(rightpanel);
-
+        frame.add(leftPanel);
 
         frame.setVisible(true);
 
@@ -130,10 +224,7 @@ public class HomePage {
 
 
     public static void main(String[] args) {
-        homePage = new HomePage();
+        new HomePage();
     }
-    
 
-    
-    
 }
